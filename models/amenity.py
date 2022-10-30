40bcedbf1bd5c051968ca4f20d67765f43f528e5
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-10-30 23:35:34 +0100

    Removed function definition

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,3 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """class Amenity"""
     name = ""
-
-	def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
